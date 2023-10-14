70db74fce8fe44e0d89cc3a77de10e477e8dbba0
EimanEltayeb <em_moh092@hotmail.com>
2023-10-14 06:57:12 -0700

    main.h

---
+++ main.h
@@ -0,0 +1,28 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+int write_str(char *str, int count);
+int write_char(char c);
+int _printf(const char *format, ...);
+int _strlen(char *s);
+void print_number(int n, int count);
+int print_binary(int n, int count);
+
+/**
+ * struct search - searches for certain values
+ * @f: function pointer
+ * @search: pointer
+*/
+typedef struct search
+{
+	char *search;
+	int (*f)(void);
+} search_t;
+
+#endif
