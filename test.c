f1ee0dbe1599dc75a300cc1451360e596fd3cefd
EimanEltayeb <em_moh092@hotmail.com>
2023-10-14 07:28:53 -0700

    test.c

---
+++ test.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - used for testing
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	_printf("%i in binary is %b\n", 98,  98);
+	_printf("%i in binary is %       d", 98, 98);
+	return (0);
+}
