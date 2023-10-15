7fd3a7b9e97d6813e35418170de7dba5697ed26b
EimanEltayeb <em_moh092@hotmail.com>
2023-10-14 23:29:24 -0700

    main.h

--- main.h
+++ main.h
@@ -12,10 +12,9 @@ int write_char(char c);
 int _printf(const char *format, ...);
 int _strlen(char *s);
 int print_number(int n, int count);
-int print_binary(int n, int count);
-int search_advanced(const char c, int count);
-int search(va_list ap, const char c, int count);
-
+int print_binary(int n);
+int switch_function(va_list, char c);
+int advanced_function(va_list, char c);
 /**
  * struct search - searches for certain values
  * @f: function pointer
