b68251a813e69ffd65fead43def17ba87295b86e
EimanEltayeb <em_moh092@hotmail.com>
2023-10-14 17:46:07 -0700

    advanced.c

---
+++ advanced.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_binary - prints a number in binary
+ * @n: number to be printed
+ * @count: count of digits.
+ *
+ * Return: number of digits
+ */
+
+int print_binary(int n, int count)
+{
+	if (n != 0)
+	{
+		print_binary(n / 2, count);
+		if (n % 2 == 0)
+		{
+			write_char(0 + '0');
+			count++;
+		}
+		else
+		{
+			write_char(1 + '0');
+			count++;
+		}
+	}
+	count++;
+	return (count);
+}
