841a7ef53f4dda3f6148d5ab913a0fda4e9b32cf
EimanEltayeb <em_moh092@hotmail.com>
2023-10-16 17:33:41 -0700

    handlers

---
+++ third_advanced.c
@@ -0,0 +1,61 @@
+#include "main.h"
+/**
+ * switch_flags - searches for flags
+ * @ap: va list
+ * @format: format
+ * @i: counter
+ * Return: count
+ */
+int switch_advanced_three(va_list ap, const char *format, int i)
+{
+	int d, count = 0, j;
+	unsigned int u;
+	char b, c;
+
+	b = format[i];
+	c = format[i + 1];
+	for (j = i; b != '\0'; j++)
+	{
+		if ((b == '+') && ((c == 'd') || (c == 'i')))
+		{
+			d = va_arg(ap, int);
+			if (d >= 0)
+			{
+				write_char('+');
+				count += 1;
+			}
+			count += print_number(d);
+			break;
+		}
+		else if ((b == ' ') && ((c == 'd') || (c == 'i')))
+		{
+			d = va_arg(ap, int);
+			if (d >= 0)
+			{
+				 write_char(' ');
+				 count += 1;
+			}
+			count += print_number(d);
+			break;
+		}
+		else if ((b == '#') && ((c == 'X') || (c == 'x')))
+		{
+			u = va_arg(ap, int);
+			write_char('0');
+			write_char('x');
+			count += 2;
+			if (c == 'x')
+				count += print_s_hexa(u);
+			if (c == 'X')
+				count += print_c_hexa(u);
+		}
+		else if ((b == '#') && (c == 'o'))	
+			{
+				u = va_arg(ap, int);
+				write_char('0');
+				count += print_octal(u) + 1;
+				break;
+			}
+		}
+	return (count);
+}
