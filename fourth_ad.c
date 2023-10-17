96660d053c80b1393d2a3bb30515795b41aad6a5
EimanEltayeb <em_moh092@hotmail.com>
2023-10-17 00:51:48 -0700

    edited

--- fourth_ad.c
+++ fourth_ad.c
@@ -1,25 +1,29 @@
 #include "main.h"
 /**
- *
- *
+ * switch_advanced_long - a function to print long int
+ * @ap: valist
+ * @format: format
+ * @i: format counter
+ * @i_p: pointer to i in previous loop.
+ * Return: count
  */
 int switch_advanced_long(va_list ap, const char *format, int i, int *i_p)
 {
 	long int d;
 	int count = 0;
-	long unsigned int u;
+	unsigned long int u;
 	char b, c;
 
 	*i_p = 0;
 	b = format[i];
 	c = format[i + 1];
-	if ((b == 'l') && (c == 'd'))
+	if (((b == 'l') && (c == 'd')) || (c == 'i'))
 	{
 		d = va_arg(ap, long int);
 		count += print_long(d);
 		*i_p += 1;
 	}
-	else if ((b == 'l') && (c == 'i'))
+	else if ((b == 'l') && (c == 'u'))
 	{
 		u = va_arg(ap, long int);
 		count += print_long_unsign(u);
