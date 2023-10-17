6ca2ace256a710ca7be32ccac1b90d460c602053
EimanEltayeb <em_moh092@hotmail.com>
2023-10-16 20:34:11 -0700

    temp file

--- third_advanced.c
+++ third_advanced.c
@@ -8,17 +8,24 @@
  */
 int switch_advanced_three(va_list ap, const char *format, int i, int *i_p)
 {
-	int d, count = 0, j;
+	int d, count = 0, j = i + 1;
 	unsigned int u;
-	char b, c;
+	char b, c, jc;
 	
 	*i_p = 0;
 	b = format[i];
 	c = format[i + 1];
-	for (j = i; b != '\0'; j++)
-	{
-		if (((b == '+') || (b == ' ')) && ((c == 'd') || (c == 'i')))
+	jc = format[j];
+
+		if ((b == '+') || (b == ' ' || b == '0')) /*&& ((c == 'd') || (c == 'i')))*/
 		{
+			*i_p += 1;
+			for (j = (i + 1); !((jc == 'd') || (jc == 'i')); j++)
+			{
+				*i_p += 1;
+				if (jc == '\0')
+					return (-1);
+			}
 			d = va_arg(ap, int);
 			if (d >= 0)
 			{
@@ -30,20 +37,7 @@ int switch_advanced_three(va_list ap, const char *format, int i, int *i_p)
 				*i_p += 1;
 			}
 			count += print_number(d);
-			break;
 		}
-		/*}
-		else if ((b == ' ') && ((c == 'd') || (c == 'i')))
-		{
-			d = va_arg(ap, int);
-			if (d >= 0)
-			{
-				 write_char(' ');
-				 count += 1;
-			}
-			count += print_number(d);
-			break;
-		}*/
 		else if ((b == '#') && ((c == 'X') || (c == 'x')))
 		{
 			u = va_arg(ap, int);
@@ -55,7 +49,6 @@ int switch_advanced_three(va_list ap, const char *format, int i, int *i_p)
 			if (c == 'X')
 				count += print_c_hexa(u);
 			*i_p += 1;
-			break;
 		}
 		else if ((b == '#') && (c == 'o'))
 		{
@@ -63,8 +56,6 @@ int switch_advanced_three(va_list ap, const char *format, int i, int *i_p)
 			write_char('0');
 			count += print_octal(u) + 1;
 			*i_p += 1;
-			break;
 		}
-	}
 	return (count);
 }
