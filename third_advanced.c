c57917203ff00e3d03e1b3976bb4730ef60ea669
EimanEltayeb <em_moh092@hotmail.com>
2023-10-17 12:21:13 -0700

    updates

--- third_advanced.c
+++ third_advanced.c
@@ -51,31 +51,26 @@ int switch_advanced_three(va_list ap, const char *format, int i, int *i_p)
  */
 int switch_ad_unsign(va_list ap, const char *format, int i, int *i_p)
 {
-	int count = 0, j;
+	int count = 0;
 	char b, c;
 	unsigned int u;
 
-
 	b = format[i];
 	c = format[i + 1];
-	for (j = i; b != '\0'; j++)
+	if (((b == '+') || (b == ' ')) && (c == 'u'))
 	{
-		if (((b == '+') || (b == ' ')) && (c == 'u'))
-		{
-			u = va_arg(ap, int);
-			if (b == '+')
-				write_char('+');
-			if (b == ' ')
-				write_char(' ');
-			count++;
-			*i_p += 1;
-			count += print_unsign(u);
-			break;
-		}
-		else
-		{
-			count += switch_ad_cont(ap, format, i, i_p);
-		}
+		u = va_arg(ap, int);
+		if (b == '+')
+			write_char('+');
+		if (b == ' ')
+			write_char(' ');
+		count++;
+		*i_p += 1;
+		count += print_unsign(u);
+	}
+	else
+	{
+		count += switch_ad_cont(ap, format, i, i_p);
 	}
 	return (count);
 }
@@ -89,41 +84,35 @@ int switch_ad_unsign(va_list ap, const char *format, int i, int *i_p)
  */
 int switch_ad_cont(va_list ap, const char *format, int i, int *i_p)
 {
-	int count = 0, j;
+	int count = 0;
 	unsigned int u;
 	char b, c;
 
 	*i_p = 0;
 	b = format[i];
 	c = format[i + 1];
-	for (j = i; b != '\0'; j++)
+	if ((b == '#') && ((c == 'X') || (c == 'x')))
 	{
-		if ((b == '#') && ((c == 'X') || (c == 'x')))
-		{
-			u = va_arg(ap, int);
-			write_char('0');
-			write_char('x');
-			count += 2;
-			if (c == 'x')
-				count += print_s_hexa(u);
-			if (c == 'X')
-				count += print_c_hexa(u);
-			*i_p += 1;
-			break;
-		}
-		else if ((b == '#') && (c == 'o'))
-		{
-			u = va_arg(ap, int);
-			write_char('0');
-			count += print_octal(u) + 1;
-			*i_p += 1;
-			break;
-		}
-		else
-		{
-			count += switch_advanced_long(ap, format, i, i_p);
-			break;
-		}
+		u = va_arg(ap, int);
+		write_char('0');
+		write_char('x');
+		count += 2;
+		if (c == 'x')
+			count += print_s_hexa(u);
+		if (c == 'X')
+			count += print_c_hexa(u);
+		*i_p += 1;
+	}
+	else if ((b == '#') && (c == 'o'))
+	{
+		u = va_arg(ap, int);
+		write_char('0');
+		count += print_octal(u) + 1;
+		*i_p += 1;
+	}
+	else
+	{
+		count += switch_advanced_long(ap, format, i, i_p);
 	}
 	return (count);
 }
