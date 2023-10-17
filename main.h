ce63121a2ae67fa6a57f530e13febc9037958e36
EimanEltayeb <em_moh092@hotmail.com>
2023-10-16 21:54:57 -0700

    main.h

--- main.h
+++ main.h
@@ -27,6 +27,7 @@ int search_advanced_3(const char c, va_list ap);
 int print_reversed(char *str);
 int print_R(char *str);
 int switch_advanced_three(va_list ap, const char *format, int i, int *i_p);
+int switch_ad_cont(va_list ap, const char *format, int i, int *i_p);
 
 /**
  * struct search - searches for certain values
